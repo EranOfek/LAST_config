ab98a0289996a18308a315def33d7798b7800576
Enrico Segre <enrico.segre@weizmann.ac.il>
2021-06-15 18:24:38 +0300

    remove old +configfile functions

--- +configfile/pathname.m
+++
@@ -1,8 +0,0 @@
-function Path=pathname
-% Return path name in which the configuration files resides
-
-functionpath=fileparts(mfilename('fullpath'));
-
-% for now the configuration files are in a subdir of LAST_config, %  but it
-%  may be sensible to move them to a global common directory
-Path=fullfile(functionpath,'..','config');
