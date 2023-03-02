5d7f26c459f93c9377abbb2312817cf6ef951597
Enrico Segre <enrico.segre@weizmann.ac.il>
2023-03-02 12:10:40 +0200

    completed script create_new_unit_configs.sh

--- create_new_unit_configs.sh
+++ create_new_unit_configs.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# the present version creates configuration files using the mastrolindo branch
+#  Id convention. TODO, a flag for creating files (or links?) using the
+#  Locator.CanonicalLocation naming convention of branch webapi_transition
+#  (If I count correctly, this should affect only obs. configurations, not inst.,
+#   except for inst.tinycontrolIPpowerSocket.create and .destroy)
+
 instructions()
 {
   echo "This script creates new configuration files for all of the components"
@@ -10,6 +16,7 @@ instructions()
   echo "       MountName is the string returned by instXerxesMount.PhysicalId"
   echo "       C1_SN#...C4_SN# are the full serial numbers of the four cameras"
   echo "       (17 alphanumeric digits, without the QHY600M- prefix)"
+  echo "       A way to retrieve them in Matlab: Q=inst.QHYccd; Q.allQHYCameraNames"
 }
 
 if [[ -z $1 || $1 == "-h" || $1 == "-?" ]]; then
@@ -19,11 +26,11 @@ fi
 
 unit=`printf "%02d" $1`
 
-echo "Creating configuration files for unit $unit"
+echo "Creating configuration files for unit $unit :"
 
 ##################
 
-echo "  - Creating power switches configurations"
+echo "  - creating power switches configurations"
 
 ## TODO: a flag for changing between staging and production IP addresses
 subnet=3; # 2 in production
@@ -60,7 +67,7 @@ if [[ -z $2 ]]; then
    exit
 fi
 
-echo "  - Creating inst.XerxesMount...connect configuration"
+echo "  - creating inst.XerxesMount...connect configuration"
 
 tail +2 config/inst.XerxesMount/inst.XerxesMount.#TEMPLATE.connect.yml >\
         config/inst.XerxesMount/inst.XerxesMount.$2.connect.yml
@@ -73,15 +80,84 @@ if [[ -z $3 || -z $4 || -z $5 || -z $6 ]]; then
    exit
 fi
 
-echo "  - Creating four inst.QHYccd...connect configurations"
+echo "  - creating four inst.QHYccd...connect configurations"
 
-tail +2 config/inst.QHYccd/inst.QHYccd.QHY600M-#TEMPLATE.connect.yml >\
-        config/inst.QHYccd/inst.QHYccd.QHY600M-$3.connect.yml
-tail +2 config/inst.QHYccd/inst.QHYccd.QHY600M-#TEMPLATE.connect.yml >\
-        config/inst.QHYccd/inst.QHYccd.QHY600M-$4.connect.yml
-tail +2 config/inst.QHYccd/inst.QHYccd.QHY600M-#TEMPLATE.connect.yml >\
-        config/inst.QHYccd/inst.QHYccd.QHY600M-$5.connect.yml
-tail +2 config/inst.QHYccd/inst.QHYccd.QHY600M-#TEMPLATE.connect.yml >\
-        config/inst.QHYccd/inst.QHYccd.QHY600M-$6.connect.yml
+for s in $3 $4 $5 $6; do
+    tail +2 config/inst.QHYccd/inst.QHYccd.QHY600M-#TEMPLATE.connect.yml >\
+           config/inst.QHYccd/inst.QHYccd.QHY600M-$s.connect.yml
+done
 
+echo "  - creating four obs.camera...create configurations"
+
+tail +3 config/obs.camera/obs.camera.#TEMPLATE.create.yml |\
+        sed -e s/XXXXXXXXXXXXXXXXX/$3/  -e s/lastXX/last$unit\e/g \
+            -e s/dataX/data1/ -e s/NN/1/ -e s/NESW/NE/ > \
+        config/obs.camera/obs.camera.$unit\_1_1.create.yml
+tail +3 config/obs.camera/obs.camera.#TEMPLATE.create.yml |\
+        sed -e s/XXXXXXXXXXXXXXXXX/$4/  -e s/lastXX/last$unit\e/g \
+            -e s/dataX/data2/ -e s/NN/2/ -e s/NESW/SE/ > \
+        config/obs.camera/obs.camera.$unit\_1_2.create.yml
+tail +3 config/obs.camera/obs.camera.#TEMPLATE.create.yml |\
+        sed -e s/XXXXXXXXXXXXXXXXX/$5/  -e s/lastXX/last$unit\w/g \
+            -e s/dataX/data1/ -e s/NN/3/ -e s/NESW/SW/ > \
+        config/obs.camera/obs.camera.$unit\_1_3.create.yml
+tail +3 config/obs.camera/obs.camera.#TEMPLATE.create.yml |\
+        sed -e s/XXXXXXXXXXXXXXXXX/$6/  -e s/lastXX/last$unit\w/g \
+            -e s/dataX/data2/ -e s/NN/4/ -e s/NESW/NW/ > \
+        config/obs.camera/obs.camera.$unit\_1_4.create.yml
+
+###############
+
+echo "  - creating four obs.focuser...create configurations"
+
+tail +3 config/obs.focuser/obs.focuser.#TEMPLATE.create.yml | \
+   sed -e s/XXXX/pci-0000:48:00.0-usb-0:2:1.0/ > \
+   config/obs.focuser/obs.focuser.$unit\_1_1.create.yml
+tail +3 config/obs.focuser/obs.focuser.#TEMPLATE.create.yml | \
+   sed -e s/XXXX/pci-0000:49:00.0-usb-0:2:1.0/ > \
+   config/obs.focuser/obs.focuser.$unit\_1_2.create.yml
+tail +3 config/obs.focuser/obs.focuser.#TEMPLATE.create.yml | \
+   sed -e s/XXXX/pci-0000:49:00.0-usb-0:2:1.0/ > \
+   config/obs.focuser/obs.focuser.$unit\_1_3.create.yml
+tail +3 config/obs.focuser/obs.focuser.#TEMPLATE.create.yml | \
+   sed -e s/XXXX/pci-0000:48:00.0-usb-0:2:1.0/ > \
+   config/obs.focuser/obs.focuser.$unit\_1_4.create.yml
+
+
+###############
+
+echo "  - creating obs.mount...create configuration"
+
+tail +3 config/obs.mount/obs.mount.#TEMPLATE.create.yml | \
+   sed -e "s|XXXX|\/dev\/ttyS4|" -e s/lastXX/last$unit\e/ > \
+   config/obs.mount/obs.mount.$unit\_1.create.yml
+
+###############
+
+echo "  - creating obs.unitCS...create configuration for master and four slaves"
+
+tail +2 config/obs.unitCS/obs.unitCS.#TEMPLATE_MASTER.create.yml > \
+    config/obs.unitCS/obs.UnitCS.$unit.create.yml
+
+for i in {1..4}; do
+    tail +2 config/obs.unitCS/obs.unitCS.#TEMPLATE_SLAVE.create.yml | \
+        sed -e s/XX/$unit/ -e s/NN/$i/ >  config/obs.unitCS/obs.UnitCS.$unit\_slave_$i.create.yml
+done
+
+###############
+
+echo "  - creating four obs.util.SpawnedMatlab...create configurations, for slaves"
+
+tail +2 config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.#TEMPLATE.create.yml | \
+    sed -e s/XX/$unit\e/ > \
+    config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.$unit\_slave_1.create.yml
+tail +2 config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.#TEMPLATE.create.yml | \
+    sed -e s/XX/$unit\e/ > \
+    config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.$unit\_slave_2.create.yml
+tail +2 config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.#TEMPLATE.create.yml | \
+    sed -e s/XX/$unit\w/ > \
+    config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.$unit\_slave_3.create.yml
+tail +2 config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.#TEMPLATE.create.yml | \
+    sed -e s/XX/$unit\w/ > \
+    config/obs.util.SpawnedMatlab/obs.util.SpawnedMatlab.$unit\_slave_4.create.yml
 
