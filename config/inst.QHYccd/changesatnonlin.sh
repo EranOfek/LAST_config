f773c6305408dce7a038d7ef58b36ca4dcfd85e5
Enrico Segre <enrico.segre@weizmann.ac.il>
2023-03-12 11:34:42 +0200

    update inst.QHYccd configs with values for FITSHeader as per overview paper

---
+++ config/inst.QHYccd/changesatnonlin.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+for f in `ls *connect.yml`
+do
+    head -9 $f > temp.yml
+    tail +19 temp.yml >> $f
+    cat $f | sed -e "s/# Measured parameters like above, but repeated here for the current mode,//" \
+                 -e "s/#  not looked up lookup from above/# measured values as per Table 4 of the Overview paper/" \
+                 -e "s/\['GAIN',0.9\]/\['GAIN',0.75\]/" \
+                 -e "s/\['READNOI',4.5\]/\['READNOI',3.0\]/" \
+                 -e "s/\['SATURVAL',60000\]/\['SATURVAL',62000\]/" \
+                 -e "s/\['DARKCUR',0.01\]/\['DARKCUR',0.008\]/" \
+                 -e "s/\['NONLIN',50000\]/\['NONLIN',62000\]/" > temp.yml
+    cp temp.yml $f
+done
+
+rm temp.yml
